73adcc098e6ac1664d38369fcc80114b53df87fd
Cassandra <Cassandra>
2019-04-06 12:45:40 -0500

    the frick

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -24,7 +25,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job job = jobData.findById(id);
+        model.addAttribute("job", job);
         return "job-detail";
     }
 
@@ -41,7 +43,14 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
-
+        if (!errors.hasErrors()) {
+            JobData jobData = JobData.getInstance();
+            Job job = new Job(jobForm.getName(), jobForm.getEmployers(),jobForm.getLocations(),jobForm.getPositionTypes(),jobForm.getCoreCompetencies());
+            jobData.add(job);
+            return "redirect:/job/?id=" + job.getId();
+        }
+        model.addAttribute("errors", errors);
+        return "new-job";
     }
+
 }
